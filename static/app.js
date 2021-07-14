309a51cd3eebbda7cbe582f2c8c131aa1cddba8a
Kalicia Adams <kally1009@kalicia-laptop.localdomain>
2021-07-14 09:35:25 -0600

    Added basic search feature

--- static/app.js
+++ static/app.js
@@ -1,12 +1,36 @@
 var app = new Vue({
-    el: "app",
-    
+    el: "#app",
+    data: {
+        search_string:"",
+        products: [
+            {
+                "title":"Single Desert Mountain",
+                "url":"",
+                "image":"../images/desert_mountain.jpg",
+                "description":"Desert Mountain"
+            },
+            {
+                "title":"California Waves",
+                "url":"",
+                "image":"../images/wave.jpg",
+                "descriptioin":"Ocean Waves in California USA"
+            }
+        ]
 
+    },
+    computed: {
+        filteredArticles: function(){
+            var product_array = this.products;
+            var searchString=this.searchString
+            searchString = searchString.trim().toLowerCase();
 
+            product_array= product_array.filter(function(item){
+                if(item.title.toLowerCase().indexOf(searchString)!==-1){
+                    return item
+                }
 
-
-
-
-
-
-})
+            })
+            return product_array
+        }
+   }
+});
